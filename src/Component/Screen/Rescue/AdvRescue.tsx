aeb781806fe5b106c72261ca18d50d012c76f6e7
Jaeyoung Song <sjy920404@naver.com>
2019-08-10 21:19:53 +0900

    edit warning

--- src/Component/Screen/Rescue/AdvRescue.tsx
+++ src/Component/Screen/Rescue/AdvRescue.tsx
@@ -4,10 +4,10 @@ export default class AdvRescue extends Component {
   render() {
     return (
       <div>
-        <img
+        {/* <img
           src="https://youthpress.net/xe/files/attach/images/9790/791/477/68c6b3b95ce7b11d0697e6efb16fdee6.jpg"
           style={{ width: '70%', height: '70%', margin: '4%' }}
-        ></img>
+        ></img> */}
       </div>
     );
   }
